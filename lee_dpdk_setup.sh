6a33364382d89b3f34a5368b08c9673e794a8477
Alan-nsfocus-vmware-ubuntu <e9ternity@gmail.com>
2020-11-23 14:00:22 +0800

    modify dpdk setup.sh

--- lee_dpdk_setup.sh
+++ lee_dpdk_setup.sh
@@ -1,5 +1,6 @@
 # !/bin/sh
 
+RTE_IGB_UIO="/root/kmods-dpdk/linux/igb_uio"
 RTE_DEV_TOOL="$RTE_SDK/usertools/dpdk-devbind.py "
 
 # this is dpdk setup in vmware
@@ -7,16 +8,24 @@ RTE_DEV_TOOL="$RTE_SDK/usertools/dpdk-devbind.py "
 # first : config hugepages (already config on boot)
 
 # second : insmod dpdk uio
+cd $RTE_IGB_UIO
+rmmod igb_uio.ko
+make > /dev/null 2>&1
 sudo modprobe uio
-sudo insmod $RTE_SDK/$RTE_TARGET/kmod/igb_uio.ko
+sudo insmod igb_uio.ko
+cd -
 
 # eth down
-ifconfig eth37 down
-ifconfig eth38 down
+ifconfig ens37 down > /dev/null 2>&1
+ifconfig ens38 down > /dev/null 2>&1
+
+# sleep 3 seconds
+sleep 3
 
 # bind network device
-# first show
-eval $RTE_DEV_TOOL "--status"
 
 # second bind
-eval $RTE_DEV_TOOL "-b igb_uio 0000:02:05.0 0000:02:06.0"
+eval  python3 $RTE_DEV_TOOL "-b igb_uio 0000:02:05.0 0000:02:06.0"
+
+echo "\n======== after dev bind========\n"
+eval python3 $RTE_DEV_TOOL "--status"
