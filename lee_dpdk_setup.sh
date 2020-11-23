30a453d02148303b702f9df5448a11dfb8630878
Alan-3900x-ubuntu <e9ternity@gmail.com>
2020-11-23 23:23:14 +0800

    modify dpdk setup

--- lee_dpdk_setup.sh
+++ lee_dpdk_setup.sh
@@ -1,23 +1,40 @@
 # !/bin/sh
 
-RTE_IGB_UIO="/root/kmods-dpdk/linux/igb_uio"
+RTE_SDK="/root/dpdk"
+RTE_TARGET="x86_64-native-linuxapp-gcc"
+RTE_IGB_UIO="/root/dpdk/x86_64-native-linuxapp-gcc/kernel/linux/igb_uio"
+RTE_KNI="/root/dpdk/x86_64-native-linuxapp-gcc/kernel/linux/kni"
 RTE_DEV_TOOL="$RTE_SDK/usertools/dpdk-devbind.py "
 
 # this is dpdk setup in vmware
 
-# first : config hugepages (already config on boot)
+# compile dpdk
+cd $RTE_SDK
+rm -rf $RTE_TARGET
+meson -Dexamples=all -Denable_kmods=true $RTE_TARGET
+cd $RTE_TARGET
+ninja
+ninja install
+ldconfig
+cd ~
 
-# second : insmod dpdk uio
+# insmod dpdk uio
 cd $RTE_IGB_UIO
 rmmod igb_uio.ko
-make > /dev/null 2>&1
-sudo modprobe uio
-sudo insmod igb_uio.ko
-cd -
+modprobe uio
+insmod igb_uio.ko
+cd ~
+
+cd $RTE_KNI
+rmmod rte_kni.ko
+insmod rte_kni.ko
+cd ~
 
 # eth down
-ifconfig ens37 down > /dev/null 2>&1
 ifconfig ens38 down > /dev/null 2>&1
+ifconfig ens39 down > /dev/null 2>&1
+ifconfig ens40 down > /dev/null 2>&1
+ifconfig ens41 down > /dev/null 2>&1
 
 # sleep 3 seconds
 sleep 3
@@ -25,7 +42,7 @@ sleep 3
 # bind network device
 
 # second bind
-eval  python3 $RTE_DEV_TOOL "-b igb_uio 0000:02:05.0 0000:02:06.0"
+eval  python3 $RTE_DEV_TOOL "-b igb_uio 0000:02:06.0 0000:02:07.0 0000:02:08.0 0000:02:09.0"
 
 echo "\n======== after dev bind========\n"
 eval python3 $RTE_DEV_TOOL "--status"
